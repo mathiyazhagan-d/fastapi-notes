454f8d42e817203b3562948f759ee64a2257bf17
mathiyazhagan-d <116990455+mathiyazhagan-d@users.noreply.github.com>
2025-02-05 14:03:25 +0530

    ignore

--- app/database.py
+++ app/database.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 from app.config import MONGO_URI
 
 client = MongoClient(MONGO_URI)
-db = client["notes-py"]  # Change "notes_app" to your preferred database name
+db = client["notes-py"]  
